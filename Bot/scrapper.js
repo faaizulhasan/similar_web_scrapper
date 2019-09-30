09a8041df0855610c6d2d20c17c364d439ec584b
Faiz <faiz@fomarkmedia.com>
2019-09-30 16:09:44 +0500

    #checking for owler

--- Bot/scrapper.js
+++ Bot/scrapper.js
@@ -46,7 +46,6 @@ class Scrapper {
 
     async visitWebsite() {
         await this.page.goto(this.config.url);
-        console.log('open website...')
         // await this.page.waitForNavigation();
     }
 
